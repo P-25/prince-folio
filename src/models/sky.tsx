bb59454ec03859d4d0ed40758c7052ea2d1bd037
Prince Sharma <prince@appycodes.com>
2024-08-03 12:30:15 +0530

    fix: Sky component

--- src/models/sky.tsx
+++ src/models/sky.tsx
@@ -1,27 +1,24 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 
-import { useGLTF, useFrame } from "@react-three/drei";
 import { useRef } from "react";
-import { Mesh } from "three";
-
-// Define the type for props
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 interface SkyProps {
   isRotating: boolean;
 }
 
+// 3D Model from: https://sketchfab.com/3d-models/phoenix-bird-844ba0cf144a413ea92c779f18912042
 const Sky: React.FC<SkyProps> = ({ isRotating }) => {
   const sky = useGLTF("./assets/sky.glb");
-  const skyRef = useRef<Mesh>(null); // Specify Mesh as the type for the ref
+  const skyRef = useRef<Mesh>(null);
 
   // Note: Animation names can be found on the Sketchfab website where the 3D model is hosted.
   // It ensures smooth animations by making the rotation frame rate-independent.
   // 'delta' represents the time in seconds since the last frame.
   useFrame((_, delta) => {
     if (isRotating) {
-      if (skyRef.current) {
-        skyRef.current.rotation.y += 0.25 * delta; // Adjust the rotation speed as needed
-      }
+      skyRef.current.rotation.y += 0.25 * delta; // Adjust the rotation speed as needed
     }
   });
 
